ca176d63da2c8904f0b0a3228e33c6b2e8eb59b2
Andreas Christiansen <andycchhrr@gmail.com>
2023-12-03 18:33:23 +0100

    fixed

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-function openDropdown(){
-    const moreDropdown = document.querySelector('#menu__dropdown');
-    moreDropdown.style.display = 'flex';
-    moreDropdown.style.flexDirection = 'column';
-    moreDropdown.style.alignItems = 'flex-start';
+// NAV drop down script
+function openDropdown() {
+  const moreDropdown = document.querySelector("#menu__dropdown");
+  moreDropdown.style.display = "flex";
+  moreDropdown.style.flexDirection = "column";
+  moreDropdown.style.alignItems = "flex-start";
 }
 
-function closeDropdown(){
-    const moreDropdown = document.querySelector('#menu__dropdown');
-    moreDropdown.style.display = 'none';
+function closeDropdown() {
+  const moreDropdown = document.querySelector("#menu__dropdown");
+  moreDropdown.style.display = "none";
 }
 
+// Pop-up script
 document.getElementById("open-map").addEventListener("click", function () {
-    document.getElementById("mapPopUp").style.display = "flex";
-    document.documentElement.scrollTop = document.documentElement.scrollHeight;
-    document.getElementById("focus").focus();
+  document.getElementById("mapPopUp").style.display = "flex";
+  document.documentElement.scrollTop = document.documentElement.scrollHeight;
+  document.getElementById("focus").focus();
 });
 
 document.getElementById("close-map").addEventListener("click", function () {
-    document.getElementById("mapPopUp").style.display = "none";
+  document.getElementById("mapPopUp").style.display = "none";
 });
 
+// Error msg script
+const emailInput = document.querySelector("#email");
+const fnameInput = document.querySelector("#fname");
+const lnameInput = document.querySelector("#lname");
+const emailError = document.querySelector("#emailErrorMsg");
+const fnameError = document.querySelector("#fnameErrorMsg");
+const lnameError = document.querySelector("#lnameErrorMsg");
 
-
-const emailInput = document.querySelector('#email');
-const emailError = document.querySelector('#errorMsg');
-
-emailInput.addEventListener('input', () => {
+const validateEmail = () => {
   const emailPattern = /^[^\s@]+@[^\s@]+/;
+  emailError.style.display =
+    emailPattern.test(emailInput.value) || emailInput.value === ""
+      ? "none"
+      : "block";
+};
 
-  emailError.style.display = emailPattern.test(emailInput.value) || emailInput.value === '' ? 'none' : 'block';
-});
+const validateName = (input, errorElement) => {
+  const namePattern = /^[a-zA-Z]+$/;
+  errorElement.style.display =
+    namePattern.test(input.value) || input.value === "" ? "none" : "block";
+};
+
+emailInput.addEventListener("input", validateEmail);
+fnameInput.addEventListener("input", () =>
+  validateName(fnameInput, fnameError)
+);
+lnameInput.addEventListener("input", () =>
+  validateName(lnameInput, lnameError)
+);
